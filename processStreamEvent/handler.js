2f0993954174e1963dacc5a8ec94564183cdc70a
Ian Serlin <ian@ianserlin.com>
2016-05-03 18:36:08 +0900

    adds sample event json and outputs `dynamodb` to the console

--- processStreamEvent/handler.js
+++ processStreamEvent/handler.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const util = require('util');
+
 module.exports.handler = function(event, context, cb) {
+  console.log(util.inspect(event.dynamodb, false, null));
   return cb(null, {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
+    message: 'Go Serverless DynamoDB Streams! Your Lambda function executed successfully! Actually you do not need to return anything for an event-based Lambda.'
   });
 };
